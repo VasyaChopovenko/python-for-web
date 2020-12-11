13d0ed8077275b2d63d3b70336fd964ef4aa433c
VasyaChopovenko <vasyachopovenko@gmail.com>
2020-12-11 11:27:41 +0300

    Add button

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.resume, name='resume-en'),
+    path('', views.resume, name='resume'),
     path('resume-en', views.resume_en, name='resume-en'),
     path('resume-ua', views.resume_ua, name='resume-ua'),
 ]
